9f7229f1ac500a956f35a2e020f9d3268a04d10c
Elastic Email <support@elasticemail.com>
2017-04-24 15:12:00 +0200

    Add project files.

---
+++ ElasticEmail.WebApiClient.Generator-multiplatform/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElasticEmail.WebApiClient.Generator_multiplatform
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
